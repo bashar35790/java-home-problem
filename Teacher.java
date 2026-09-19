10e4eef68a8cea17154a496d1be08c7b73ccd44e
Abul Bashar <bashar35790@gmail.com>
2026-09-19 21:15:04 +0600

    added constractor

--- Teacher.java
+++ Teacher.java
@@ -10,10 +10,17 @@ public class Teacher {
         this.phoneNumber = phoneNumber;
     }
 
+    Teacher(String name, String gender, int phoneNumber) {
+        this.name = name;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+
     void getInfo() {
         System.out.println("Teacher Name: " + name);
         System.out.println("Teacher Gender: " + gender);
         System.out.println("Teacher Phone Number: " + phoneNumber);
+        System.out.println("\n\n");
     }
-  
+
 }
--- TeacherTester.java
+++ TeacherTester.java
@@ -2,10 +2,8 @@ public class TeacherTester {
     public static void main(String[] args){
         Teacher teacher1;
         Teacher teacher2;
-        teacher1 = new Teacher();
-        teacher2 = new Teacher();
-        teacher1.setInfo("John", "Male", 1234567890);
-        teacher2.setInfo("Jane", "Female", 987654321);
+        teacher1 = new Teacher("John", "Male", 1234567890);
+        teacher2 = new Teacher("Jane", "Female", 987654321);
 
         teacher1.getInfo();
         teacher2.getInfo();
